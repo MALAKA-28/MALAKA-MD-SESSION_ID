b16ed888ec511955134b4ac0863fa90d45914e1a
MALAKA-28 <malakamdbot63@gmail.com>
2025-03-04 20:30:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Importing the 'pair' module
+import code from './pair.js';
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 7860;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
+
+// Resolve the current directory path in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8000;
+
+import('events').then(events => {
+    events.EventEmitter.defaultMaxListeners = 500;
+});
+
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.use('/pair', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+app.use('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
+    console.log(`YoutTube: @GlobalTechInfo\n\nGitHub: @GlobalTechInfo\n\nServer running on http://localhost:${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+export default app;
